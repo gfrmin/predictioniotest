273a17d283df5795f6de51c6be1b109478539d64
Guy Freeman <startup@gfrm.in>
2014-07-26 10:22:42 +0800

    Import data

---
+++ ada.R
@@ -0,0 +1,10 @@
+library(data.table)
+
+appopen <- fread("ada/Ada_events_app_open.csv")
+loggedmeal <- fread("ada/Ada_events_logged_meal.csv")
+loggedweight <- fread("ada/Ada_events_logged_weight.csv")
+mealapproved <- fread("ada/Ada_events_meal_approved.csv")
+receivedcomment <- fread("ada/Ada_events_received_comment.csv")
+users <- fread("ada/Ada_users.csv")
+
+
