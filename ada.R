5cc3a3a6be3ab30941adbf2fd2d7586ac2c5e563
Guy Freeman <startup@gfrm.in>
2014-08-02 10:42:51 +0800

    Latest update

--- ada.R
+++ ada.R
@@ -7,4 +7,11 @@ mealapproved <- fread("ada/Ada_events_meal_approved.csv")
 receivedcomment <- fread("ada/Ada_events_received_comment.csv")
 users <- fread("ada/Ada_users.csv")
 
-
+length(appopen$user_id)
+# [1] 194235
+length(unique(appopen$user_id))
+# [1] 1872
+nrow(users)
+# [1] 907
+sum(users$user_id %in% appopen$user_id)
+# [1] 906
